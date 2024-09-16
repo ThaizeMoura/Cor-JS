67bed62a803af2e87e9d54cb87ec3734513b0140
Thaize  Moura <thaize2002@gmail.com>
2024-09-16 00:06:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//declarando a variável constante
+const colors = ["yellow", "blue", "green"];
+console.log(colors);
+
+//verificando a primeira posição do array
+console.log(colors[0]);
+
+//adicionando duas novas cores
+colors.push ("pink", "red");
+console.log(colors);
+
+//verificando cada posição do array
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+console.log(colors[4]);
+console.log(colors[5]);
